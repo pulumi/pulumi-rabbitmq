099e9a805517f74f71a3db0f3469826d066bf74b
Lee Briggs <lee@leebriggs.co.uk>
2020-06-10 20:26:59 -0700

    prepare for pulumi mgmt

---
+++ examples/examples_nodejs_test.go
@@ -0,0 +1,31 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
+
+package examples
+
+import (
+	"path"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
+
+func getJSBaseOptions() integration.ProgramTestOptions {
+	base := getBaseOptions()
+	baseJS := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"@pulumi/rabbitmq",
+		},
+	})
+
+	return baseJS
+}
+
+func TestAccUserTs(t *testing.T) {
+	test := getJSBaseOptions().
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "user", "ts"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
